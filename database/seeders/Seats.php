76ab5ae217306b0c7b290edabf11b4d6b0d2b94e
Ramin Heshmati <r.heshmati@pixaldo.com>
2022-02-18 09:56:51 +0330

    Create Seats seeder

---
+++ database/seeders/Seats.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Seat;
+use Illuminate\Database\Seeder;
+
+class Seats extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Seat::insert(
+            ['number' => ]
+        );
+    }
+}
